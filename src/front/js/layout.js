5aee0fa5657027ed7cef462f8338e0cb11d4cede
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2025-03-27 22:38:33 +0000

    contact list

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,9 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { ContactsList } from "./component/ContactList.jsx";
+import { AddContact } from "./component/AddContact.jsx";
+import { EditContact } from "./component/EditContact.jsx";
 
 //create your first component
 const Layout = () => {
@@ -28,6 +31,9 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<ContactsList/>} path="/contacts" />
+                        <Route element={<AddContact/>} path= "/add-contact" />
+                        <Route element={<EditContact/>} path= "/edit-contact" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,7 +13,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			slug: "mimoun",
+			contacts: [],
+			currentContact: {},
+			baseURLContacts:"https://playground.4geeks.com/contact/agendas"
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -46,6 +50,80 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			setCurrentContact: (contact) => { setStore({ currentContact: contact}) },
+			getContacts: async () => {
+				const uri = `${getStore().baseURLContacts}/${getStore().slug}`;
+				const options = {
+					method: "GET"
+				}
+				const response = await fetch(uri, options);
+				if(response.status === 404){
+					getActions().createUser()
+				}
+				if (!response.ok) {
+					console.log("error:", response.status, response.statusText);
+				}
+				const data = await response.json();
+				setStore({contacts: data.contacts});
+			},
+
+			deleteContact: async (contactId) => {		
+				const uri = `${getStore().baseURLContacts}/${getStore().slug}/contacts/${contactId}`;
+				const options = {
+					method: "DELETE"
+				}
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.log("error", response.status, response.statusText);
+					return
+				}
+				getActions().getContacts();
+			},
+
+			addContact: async (dataToSend) => {
+				const uri =`${getStore().baseURLContacts}/${getStore().slug}/contacts`
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(dataToSend)
+				}
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.log('error:', response.status, response.statusText)
+					return  
+				}
+				getActions().getContacts()
+			},
+			exampleFunction: () => {getActions().changeColor(0, "green");},
+			getMessage: async () => {
+				const response = await fetch(process.env.BACKEND_URL + "api/hello")
+				if (!response.ok) {
+					console.log("Error loading message from backend", response.status, response.statusText);
+					return;
+				}
+				const data = await response.json()
+				setStore({ message: data.message })
+				// don't forget to return something, that is how the async resolves
+				return data;
+			},
+			editContact: async (id, contact) =>{
+				const uri= `${getStore().baseURLContacts}/${getStore().slug}/contacts/${id}`
+				const options = {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(contact)
+				}
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					// Tratar el error
+					return;
+				}
+				getActions().getContacts()
 			}
 		}
 	};
